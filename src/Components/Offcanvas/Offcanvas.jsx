73066653e350fc2b08b17347480b32d6766a5bfc
Michele <ercolinimichele@outlook.it>
2024-03-22 19:19:22 +0100

    Platforms e switch aggiunti anche in Offcanvas

--- src/Components/Offcanvas/Offcanvas.jsx
+++ src/Components/Offcanvas/Offcanvas.jsx
@@ -4,8 +4,9 @@ import ListGenres from "../ListGenres/ListGenres";
 import ListPlatforms from "../ListPlatforms/ListPlatforms";
 import classes from "./Offcanvas.module.css"
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DarkContext } from "../../Contexts/Contexts";
+import SwitchFilter from "../SwitchFilter/SwitchFilter";
 
 export default function Offcanvas() {
 
@@ -13,29 +14,40 @@ export default function Offcanvas() {
 
     const genres = useFetch(`https://api.rawg.io/api/genres?key=${API_KEY}&page_size=18`);
 
-    const platforms = useFetch(`https://api.rawg.io/api/platforms?key=${API_KEY}&page_size=21`);
+    const platforms = useFetch(`https://api.rawg.io/api/platforms?key=${API_KEY}&page_size=12`);
 
     const { dark } = useContext(DarkContext);
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleChecked = () => {
+        setIsChecked(!isChecked);
+    }
 
     return (
         <>
-            <button className={classes.sign  + (dark ? " bg-primaryColor" : " bg-secondaryColor")} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+            <button className={classes.sign + (dark ? " bg-primaryColor" : " bg-secondaryColor")} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
                 <IoIosArrowForward color={"var(--accent2Color)"} size="2rem" />
                 {/* <div className={classes.text}>Generi</div> Si dovrebbe vedere sull'hover, ma per smartphone/tablet risulta inutile */}
             </button>
             <div className={"offcanvas offcanvas-start " + (dark ? "dark" : "light")} data-bs-scroll="true" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
                 <div className="offcanvas-header pb-3 d-flex justify-content-around align-items-center">
-                    <h2 className="">Generi</h2>
+                    
+                        <h5 className={!isChecked ? classes.title : classes.subtitle}>Genres</h5>
+                        <span className="mx-3" onClick={handleChecked}>
+                            <SwitchFilter />
+                        </span>
+                        <h5 className={!isChecked ? classes.subtitle : classes.title}>Platforms</h5>
+                    
                     <button className={classes.sign + (dark ? " bg-primaryColor" : " bg-secondaryColor")} type="button" data-bs-dismiss="offcanvas" aria-label="Close" >
                         <IoIosArrowBack color={"var(--accent2Color)"} size="2rem" />
                     </button>
                 </div>
-                <div className={classes.wrapper}>
-                    <ListGenres genres={genres} />
-                    {/* <div className="offcanvas-header py-1">
-                        <h2 className={classes.sidebar_title}>Piattaforme</h2>
-                    </div>
-                    <ListPlatforms platforms={platforms} /> */}
+                <div className="">
+                    {!isChecked ?
+                        <ListGenres genres={genres} />
+                        :
+                        <ListPlatforms platforms={platforms} />
+                    }
                 </div>
             </div>
         </>
